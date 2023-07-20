e5faaee6026fd712f11b2a294aa4ddf1b8d2f78e
KimJJRoSY <129376888+KimJJRoSY@users.noreply.github.com>
2023-07-20 16:17:01 +0900

    Add files via upload

---
+++ 7주차/#7 [2021 UPDATE] PRACTICE MOVIE APP/components/7.5_Movie.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>
+        {/* title 클릭하면 app.js에서 정한 링크로 넘어감 + title에 링크 생김  */}
+        <Link to="/movie">{title}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.protoTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
